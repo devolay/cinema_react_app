cb77bb054efde3729bc62b337b6bd849740beab1
Dawid Stachowiak <dawid.stachowiak@o2.pl>
2021-06-26 02:03:21 +0200

    Started composing Film Page and FilmCard component, changed Confirm button location

---
+++ src/shared/constants.ts
@@ -0,0 +1,3 @@
+export const FILM_API_URL = "https://imdb8.p.rapidapi.com/auto-complete";
+export const FILM_API_KEY = "3a1dc042b4msh0754cc039478a88p12f489jsn9e604288ca35";
+export const FILM_API_HOST = "imdb8.p.rapidapi.com";
