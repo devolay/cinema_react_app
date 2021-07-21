96631046a2a97c7c1cf703a624ab84fad00a1f34
Dawid Stachowiak <dawid.stachowiak@o2.pl>
2021-07-21 20:54:56 +0200

    repaired profile button bug, refactored code

--- src/store/showtimes/index.ts
+++ src/store/showtimes/index.ts
@@ -1,22 +1,26 @@
 import * as SharedTypes from "shared/types";
 import { RootState } from "store";
+import firebase from "firebase/app";
 
 export const selectCurrentDateMovieShowtimes =
-  (date: Date, movie_id: string) =>
+  (date: firebase.firestore.Timestamp, movie_id: string) =>
   ({ firestore: { data } }: RootState) =>
     !!data.showtimes &&
-    Object.values(data.showtimes).filter((room) => {
-      const specificRoom = room as unknown as SharedTypes.Showtime;
-      const xd = specificRoom.date === date && specificRoom.movie_id === parseInt(movie_id);
+    (Object.values(data.showtimes).filter((showtime) => {
+      const specificShowtime = showtime as unknown as SharedTypes.Showtime;
+      const xd =
+        specificShowtime.date.seconds === date.seconds &&
+        specificShowtime.movie_id === parseInt(movie_id);
       return xd;
-    });
+    }) as unknown as SharedTypes.Showtime[]);
 
-export const selectCurrentDateHourShowtimes =
-  (date: Date, hour: string) =>
-  ({ firestore: { data } }: RootState) =>
-    !!data.showtimes &&
-    (Object.values(data.showtimes)
-      .find((room) => (room as unknown as SharedTypes.Showtime).date === date)
-      ?.find(
-        (room) => (room as unknown as SharedTypes.Showtime).hour === hour
-      ) as unknown as SharedTypes.Showtime[]);
+// export const selectCurrentDateHourShowtimes =
+//   (date: Date, hour: string) =>
+
+//   ({ firestore: { data } }: RootState) =>
+//     !!data.showtimes &&
+//     (Object.values(data.showtimes)
+//       .find((room) => (room as unknown as SharedTypes.Showtime).date === date)
+//       ?.find(
+//         (room) => (room as unknown as SharedTypes.Showtime).hour === hour
+//       ) as unknown as SharedTypes.Showtime[]);
